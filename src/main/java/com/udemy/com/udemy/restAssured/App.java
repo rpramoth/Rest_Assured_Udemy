7ed91989cb14c926403d7f950dd43567eeb18f9e
rpramoth <48800304+rpramoth@users.noreply.github.com>
2022-01-08 10:21:40 +0530

    first commit

---
+++ src/main/java/com/udemy/com/udemy/restAssured/App.java
@@ -0,0 +1,13 @@
+package com.udemy.com.udemy.restAssured;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
